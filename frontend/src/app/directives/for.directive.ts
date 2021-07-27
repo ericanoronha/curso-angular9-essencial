8c484ae9d7895898da114b76baaacfc73ff97579
Erica Noronha <erica.noronha@live.com>
2021-07-27 00:39:54 -0300

    exercício de diretiva estrutural com ViewContainerRef, createEmbeddedView, TemplateRef e

--- frontend/src/app/directives/for.directive.ts
+++ frontend/src/app/directives/for.directive.ts
@@ -1,19 +1,25 @@
-import { Directive, OnInit, Input } from '@angular/core';
+import {
+  Directive,
+  OnInit,
+  Input,
+  ViewContainerRef,
+  TemplateRef,
+} from "@angular/core";
 
 @Directive({
-  selector: '[myFor]'
+  selector: "[myFor]",
 })
 export class ForDirective implements OnInit {
+  @Input("myForEm") numbers: number[];
 
-  @Input('myForEm') numbers: number []
-  @Input('myForUsando') texto: string
-
-  constructor() {
-    
-  }
+  constructor(
+    private container: ViewContainerRef,
+    private template: TemplateRef<any>
+  ) {}
 
   ngOnInit(): void {
-    console.log(this.numbers)
-    console.log(this.texto)
+    for (let number of this.numbers) {
+      this.container.createEmbeddedView(this.template, { $implicit: number });
+    }
   }
 }
