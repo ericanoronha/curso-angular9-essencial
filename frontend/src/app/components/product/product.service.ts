a5bac9837c2a67545a63431e4f29d2556c9f74f8
Erica Noronha <erica.noronha@live.com>
2021-07-28 00:40:07 -0300

    cria métodos de exclusão

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -2,19 +2,18 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { Observable } from "rxjs";
-import { Product } from './product.model';
+import { Product } from "./product.model";
 
 @Injectable({
   providedIn: "root",
 })
 export class ProductService {
-
-  baseUrl = 'http://localhost:3001/products'
+  baseUrl = "http://localhost:3001/products";
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
   showMessage(msg: string): void {
-    this.snackBar.open(msg, 'x', {
+    this.snackBar.open(msg, "x", {
       duration: 3000,
       horizontalPosition: "right",
       verticalPosition: "top",
@@ -22,21 +21,25 @@ export class ProductService {
   }
 
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product)
+    return this.http.post<Product>(this.baseUrl, product);
   }
 
-  read(): Observable<Product[]>{
-    return this.http.get<Product[]>(this.baseUrl)
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
   }
 
-  readById(id: string): Observable<Product[]>{
-    const url = `${this.baseUrl}/${id}`
-    return this.http.get<Product[]>(url)
+  readById(id: string): Observable<Product[]> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Product[]>(url);
   }
 
   update(product: Product): Observable<Product> {
-    const url = `${this.baseUrl}/${product.id}`
-    return this.http.put<Product>(url, product)
+    const url = `${this.baseUrl}/${product.id}`;
+    return this.http.put<Product>(url, product);
   }
 
+  delete(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url);
+  }
 }
